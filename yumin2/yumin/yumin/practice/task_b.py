f0827eef073687a04492182b793fae1d2d3ed74e
Umi <55678673+Umi-def@users.noreply.github.com>
2022-02-10 10:22:29 +0900

    add

--- yumin2/yumin/yumin/practice/task_b.py
+++ yumin2/yumin/yumin/practice/task_b.py
@@ -110,3 +110,86 @@ class GeckoDriver:
         
         
         """
+
+
+        import os
+import requests
+import typing 
+
+class GeckoDriver:
+    ENDPOINT:str = "https://api.coingecko.com/api/v3/"
+
+    def __init__(self):
+        pass
+
+    def ping(self) ->typing.Tuple[str, dict]:
+        """
+        ping
+
+        the endpoint is https://api.coingecko.com/api/v3/ping
+
+        Returns
+        -------
+        tuple[requests.codes, dict]
+            詰まるところ,APIレスポンスのステータスコードとJSONを返したい
+
+        >>> driver = GeckoDriver()
+        >>> status_code, res = driver.ping()
+        >>> print(status_code)
+        200
+        >>> print(res)
+        {...}
+        
+
+        """
+        res = requests.get(os.path.join(self.ENDPOINT, "ping"))
+        return res.status_code, res.json()
+
+    def get_coins_list(self) -> typing.Tuple[str, dict]:
+        """
+        get_coins_list
+
+        the endpoint is https://api.coingecko.com/api/v3/coins/list
+
+        Returns
+        -------
+        tuple[requests.codes, dict]
+            詰まるところ,APIレスポンスのステータスコードとJSONを返したい
+
+        >>> driver = GeckoDriver()
+        >>> status_code, res = driver.get_coins_list()
+        >>> print(status_code)
+        200
+        >>> print(res)
+        {...}
+        """
+        res = requests.get(os.path.join(self.ENDPOINT, "coins/list"))
+        return res.status_code, res.json()
+
+    def get_coins_symbol(self) -> typing.List[str]:
+        """
+        get_coins_symbol
+            self.get_coins_listを呼び,そのJSONのidの部分だけ取り出すコードを書いてください．
+            これはJSONに慣れるためです,
+
+        Returns
+        -------
+        list[str]
+            IDの部分だけ取り出してリストを返したい,
+
+        >>> driver = GeckoDriver()
+        >>> status_code, res = driver.ping()
+        >>> print(status_code)
+        200
+        >>> print(res)
+        ['01coin', 'zoc', '01coin', '0-5x-long-algorand-token', 'algohalf', ...]
+        """
+        status_code, res=self.get_coins_list()
+        res_list=[]
+        for i in res:
+            res_list.append(i['id'])
+        return res_list
+
+driver = GeckoDriver()
+res = driver.get_coins_symbol()
+print(res)
